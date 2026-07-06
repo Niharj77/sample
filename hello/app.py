2ac82afc0fc220e3b4b6be8a3a67d74581840583
Niharj77 <niharj777@gmail.com>
2026-07-06 14:06:52 +0530

    Add files via upload

---
+++ hello/app.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+PORT = int(os.environ.get("PORT", 8080))
+VERSION = os.environ.get("APP_VERSION", "v1")
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        message = f"Hello from Jenkins + Docker CI/CD demo! Version: {VERSION}\n"
+        self.wfile.write(message.encode())
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", PORT), Handler)
+    print(f"Serving on port {PORT}, version {VERSION}")
+    server.serve_forever()
